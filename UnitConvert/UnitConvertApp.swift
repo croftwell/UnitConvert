76f5c47d93c7010f6a8e0cf3e37d354c455ec38b
croftwell <90102689+croftwell@users.noreply.github.com>
2024-02-05 23:19:44 +0300

    Initial Commit

---
+++ UnitConvert/UnitConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConvertApp.swift
+//  UnitConvert
+//
+//  Created by Ali on 5.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
